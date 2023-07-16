2439bf7b0fad3e8c8f712765a6005b90bd9e9e91
mavenson <32755302+mavenson@users.noreply.github.com>
2023-07-16 16:44:17 +0000

    dfg

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
 const lorem = "Ipsa numquam officia recusandae quae velit recusandae dignissimos. Quas cumque commodi ut nemo aliquid qui qui sequi. Dolores sit qui consectetur tempore consequatur. Nihil in ipsum eligendi debitis. Ea eaque ad dolores doloribus. Qui architecto quos ad ea facere. Hic aut ut qui. Numquam quis est deserunt quae et delectus. Perferendis fuga optio possimus ea itaque et a. Non dolores voluptatum sequi. Itaque quam nesciunt pariatur delectus accusamus tenetur labore. Nobis aperiam commodi qui excepturi tenetur. Et autem cupiditate ad voluptas expedita quia voluptate aut. Quis corrupti a quisquam aut alias.";
-
+$("")
 
 function getContent(page) {
     switch (page) {
         case "about":
-            document.getElementById('body-text').innerHTML = lorem;
-            document.documentElement.style.cssText = "--main-display: block";
+            
         case "projects":
 
         case "resume":
