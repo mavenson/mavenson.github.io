b7500e720b74a8207d2fd307b9e29760d4fae762
mavenson <32755302+mavenson@users.noreply.github.com>
2023-07-16 21:13:31 +0000

    gfd

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 const lorem = "Ipsa numquam officia recusandae quae velit recusandae dignissimos. Quas cumque commodi ut nemo aliquid qui qui sequi. Dolores sit qui consectetur tempore consequatur. Nihil in ipsum eligendi debitis. Ea eaque ad dolores doloribus. Qui architecto quos ad ea facere. Hic aut ut qui. Numquam quis est deserunt quae et delectus. Perferendis fuga optio possimus ea itaque et a. Non dolores voluptatum sequi. Itaque quam nesciunt pariatur delectus accusamus tenetur labore. Nobis aperiam commodi qui excepturi tenetur. Et autem cupiditate ad voluptas expedita quia voluptate aut. Quis corrupti a quisquam aut alias.";
 
 $(function(){
-    $("#about-content").hide();
+    $("#bio-text").hide();
 });
 
 function getContent(page) {
+    $(function(){
+        $("#landing").hide();
+    });
 
     switch (page) {
         case "about":
             $(function(){
-                $("#about-content").show();
+                $("#bio-text").show();
             });
 
         case "projects":
