5b627a2455c1643d63aa474be5b28ec53e7704e5
mavenson <32755302+mavenson@users.noreply.github.com>
2023-07-16 15:06:12 +0000

    ghj#

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ function getContent(page) {
     switch (new Date().getDay()) {
         case 0:
           page = "about";
-          return lorem;
+          lorem;
         case 1:
             page = "projects";
           break;
